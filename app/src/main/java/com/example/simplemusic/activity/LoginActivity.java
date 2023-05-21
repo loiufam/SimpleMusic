6fbc31ca2e456866b86ad9ce048384c139b490dc
loiufam <luo_yaohui88@163.com>
2023-05-21 20:39:42 +0800

    完成注册登录功能

---
+++ app/src/main/java/com/example/simplemusic/activity/LoginActivity.java
@@ -0,0 +1,90 @@
+package com.example.simplemusic.activity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.simplemusic.R;
+import com.example.simplemusic.User;
+import com.example.simplemusic.db.DBOpenHelper;
+
+import java.util.ArrayList;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private DBOpenHelper mDBOpenHelper;
+    private TextView mTvLoginactivityRegister;
+    private RelativeLayout mRlLoginactivityTop;
+    private EditText mEtLoginactivityUsername;
+    private EditText mEtLoginactivityPassword;
+    private LinearLayout mLlLoginactivityTwo;
+    private Button mBtLoginactivityLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+
+        mDBOpenHelper = new DBOpenHelper(this);
+    }
+
+    private void initView() {
+        // 初始化控件
+        mBtLoginactivityLogin = findViewById(R.id.btn_login);
+        mTvLoginactivityRegister = findViewById(R.id.btn_register);
+//        mRlLoginactivityTop = findViewById(R.id.rl_loginactivity_top);
+        mEtLoginactivityUsername = findViewById(R.id.et_user_name);
+        mEtLoginactivityPassword = findViewById(R.id.et_psw);
+//        mLlLoginactivityTwo = findViewById(R.id.ll_loginactivity_two);
+
+        // 设置点击事件监听器
+        mBtLoginactivityLogin.setOnClickListener(this);
+        mTvLoginactivityRegister.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        switch (view.getId()) {
+            // 跳转到注册界面
+            case R.id.btn_register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                finish();
+                break;
+            case R.id.btn_login:
+                String name = mEtLoginactivityUsername.getText().toString().trim();
+                String password = mEtLoginactivityPassword.getText().toString().trim();
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(password)) {
+                    ArrayList<User> data = mDBOpenHelper.getAllData();
+                    boolean match = false;
+                    for (int i = 0; i < data.size(); i++) {
+                        User user = data.get(i);
+                        if (name.equals(user.getName()) && password.equals(user.getPassword())) {
+                            match = true;
+                            break;
+                        } else {
+                            match = false;
+                        }
+                    }
+                    if (match) {
+                        Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                        finish();//销毁此Activity
+                    } else {
+                        Toast.makeText(this, "用户名或密码不正确，请重新输入", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "请输入你的用户名或密码", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
+
